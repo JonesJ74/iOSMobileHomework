d5afd1e45bf73525611fd16fdd57c45ac5722d37
JonesJ74 <98922755+JonesJ74@users.noreply.github.com>
2022-02-02 20:14:57 -0500

    Update iosClass folder
    Adds FirstApp(homework 1) and starter copy(homework 2)

---
+++ iosClass/FirstApp/FirstApp/FirstAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  FirstAppApp.swift
+//  FirstApp
+//
+//  Created by Student on 2/2/22.
+//
+
+import SwiftUI
+
+@main
+struct FirstAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
